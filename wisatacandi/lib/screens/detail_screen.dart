d513db9a87ecbccdc5d0f657baa4d2da859ae1ab
Frendy675 <frendyw7123@gmail.com>
2024-11-14 16:34:01 +0700

    Initial Commit

--- wisatacandi/lib/screens/detail_screen.dart
+++ wisatacandi/lib/screens/detail_screen.dart
@@ -45,18 +45,85 @@ class DetailScreen extends StatelessWidget {
               ),
               //Detail info
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
+                padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: Column(
                   children: [
-                    //info atas
-                    //info tengah
-                    //info bawah
+                    //Info Atas
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          candi.name,
+                          style: 
+                            const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                        ),
+                        IconButton(
+                          onPressed: (){}, 
+                          icon: const Icon(Icons.favorite_border))
+                      ],
+                    ),
+                    //Info Tengah
+                    const SizedBox(height: 16,),
+                    Row(children: [
+                      const Icon(Icons.place, color: Colors.red,),
+                      const SizedBox(width: 8,),
+                      const SizedBox(
+                        width: 70,
+                        child: Text(
+                          'Lokasi',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(': ${candi.location}')
+                    ],
+                    ),
+                    Row(children: [
+                      const Icon(Icons.calendar_month, color: Colors.blue,),
+                      const SizedBox(width: 8,),
+                      const SizedBox(
+                        width: 70,
+                        child: Text(
+                          'Dibangun',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(': ${candi.built}')
+                    ],
+                    ),
+                    Row(children: [
+                      const Icon(Icons.house, color: Colors.green,),
+                      const SizedBox(width: 8,),
+                      const SizedBox(
+                        width: 70,
+                        child: Text(
+                          'Tipe',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(': ${candi.type}')
+                    ],
+                    ),
+                    //Pemisah
+                    SizedBox(
+                      height: 16,
+                      ),
+                    Divider(
+                      color: Colors.deepPurple.shade100,
+                      ),
+                    const SizedBox(
+                      height: 16,
+                      ),
+                    //Info Bawah
                   ],
                 ),
-              )
-              //info atas
+              ),
             ],
           ),
-    );
+        );
   } 
 }
