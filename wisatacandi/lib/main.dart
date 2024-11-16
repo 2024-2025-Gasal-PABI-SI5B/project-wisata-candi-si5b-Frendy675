a7bde3f40236bb704ca1acbdb0d14e4a9e818886
Frendy675 <frendyw7123@gmail.com>
2024-11-16 20:09:04 +0700

    Profile Screen Update

--- wisatacandi/lib/main.dart
+++ wisatacandi/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:wisatacandi/data/candi_data.dart';
-import 'package:wisatacandi/screens/detail_screen.dart';
+// import 'package:wisatacandi/data/candi_data.dart';
+// import 'package:wisatacandi/screens/detail_screen.dart';
+import 'package:wisatacandi/screens/profile_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -17,9 +18,8 @@ class MainApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true
       ),
-      home: DetailScreen(
-        candi: candiList[0],
-        ),
+      // home: DetailScreen(candi: candiList[0],),
+      home: ProfileScreen(),
     );
   }
 }
